a96b2642dd7152af292a1cc09a7766e7786e1cee
jamesmurray7 <j.murray7@ncl.ac.uk>
2021-11-12 15:56:31 +0000

    Updates to kidney plots

---
+++ AIDSPlots.R
@@ -0,0 +1,27 @@
+rm(list=ls());library(tidyverse)
+source('~/Documents/Bernhardt/theme_csda.R')
+aids <- joineR::aids
+
+# Those who failed only ---------------------------------------------------
+aids %>% 
+  as_tibble %>% 
+  filter(death == 1) %>% 
+  mutate(tt = -1 * (time - obstime)) %>% 
+  ggplot(aes(x = tt, y = CD4, group = id)) + 
+  geom_line(lwd=.25,alpha=.25) + 
+  geom_smooth(aes(group=NULL), method = 'loess', formula = y~x, colour = 'black') +
+  theme_csda()
+
+# Might be too barren of a plot, comparing all trajectories?
+aids %>% distinct(id, drug) %>% count(drug)
+
+
+aids %>% 
+  as_tibble %>% 
+  ggplot(aes(x = obstime, y = CD4, group = id)) + 
+  geom_line(lwd=.25,alpha=.25) + 
+  geom_smooth(aes(group = as.factor(drug)), method = 'lm', formula = y ~ x)+
+  geom_smooth(aes(group = as.factor(drug)), method = 'lm', formula = y ~ poly(x^2)) +
+  theme_csda()
+
+# Dont think this worth pursuing.
--- KidneyPlots.R
+++ KidneyPlots.R
@@ -5,32 +5,36 @@ kidneys <- joineRML::renal
 kidneys$surv %>% distinct(id, failure) %>% count(failure) %>% mutate(p=prop.table(n)*100) #41% failure
 
 lapply(kidneys, names)
-# Define a plotting function
 
-plotter <- function(data, X, lab){
-  plot.data <- data %>% 
-    select(id, {{X}}, years, survtime = fuyears, failure) %>% 
-    filter(failure == 1) %>% 
-    filter(complete.cases(.)) %>% 
-    as_tibble %>% 
-    mutate(tt = -1 * (survtime - years))
+# Plot the two continuous biomarkers on the same plot...
+
+gfr <- kidneys$gfr %>% select(id, `level` = gfr, time = years, status = failure, survtime = fuyears) %>% mutate(bio = "Glomerular Filtration Rate (GFR)")
+hae <- kidneys$haem %>% select(id, `level` = haematocrit, time = years, status = failure, survtime = fuyears) %>% mutate(bio = "Blood Haematocrit Level")
+
+k <- rbind(gfr, hae)
+
+ktib <- k %>% 
+  filter(status == 1) %>% 
+  filter(complete.cases(.)) %>% 
+  as_tibble %>% 
+  mutate(tt = -1 * (survtime - time))
   
-  plot.data %>% 
-    ggplot(aes(x = tt, y = {{X}}, group = id)) + 
-    geom_vline(xintercept = 0, alpha = .25) + 
-    geom_line(alpha = .25, lwd = .25) + 
-    geom_smooth(aes(group = NULL), method = 'loess', formula = y ~ x, colour = 'black') + 
-    scale_y_continuous(breaks = scales::pretty_breaks(10)) +
-    scale_x_continuous(breaks = scales::pretty_breaks(10)) +
-    labs(y = lab,
-         x = 'Time (years) from kidney graft failure (0: failure time)') + 
-    theme_csda()
-}
+ktib %>% 
+  ggplot(aes(x = tt, y = level, group = id)) + 
+  geom_vline(xintercept = 0, alpha = .25) + 
+  geom_line(alpha = .25, lwd = .1) + 
+  geom_smooth(aes(group = NULL), method = 'loess', formula = y ~ x, colour = 'black') + 
+  scale_y_continuous(breaks = scales::pretty_breaks(10)) +
+  scale_x_continuous(breaks = scales::pretty_breaks(10)) +
+  facet_wrap(~bio, strip.position = 'left', scales = 'free_y') +
+  labs(y = NULL,
+       x = 'Time (years) from kidney graft failure (0: failure time)') + 
+  theme_csda() + 
+  theme(strip.placement = 'outside')
 
-gfrplot <- plotter(kidneys$gfr, gfr, 'Glomerular Filtration Rate (GFR)')
-haeplot <- plotter(kidneys$haem, haematocrit, 'Blood Haematocrit Level')
+ggsave('./KidneyContinuousTrajectories.png', width = 140, height = 60, units = 'mm', dpi = 1e3)  
 
-# Some way of plotting the binary one
+# Binary covariate (proterinuria) -----------------------------------------
 
 prot <- kidneys$prot %>% as_tibble %>% 
   filter(failure == 1) %>% 
@@ -44,7 +48,7 @@ change20s <- prot %>%
   mutate(changed  = abs(c(NA, diff(proteinuria)))) %>% 
   mutate(sum.changed = sum(changed, na.rm = T)) %>% ungroup %>% 
   distinct(id, sum.changed) %>% # pull(sum.changed) %>% sort %>% tail
-  filter(sum.changed > 20) %>% pull(id)
+  filter(sum.changed > 25) %>% pull(id)
 
 prot %>% 
   filter(id %in% change20s) %>% 
@@ -53,14 +57,14 @@ prot %>%
   ggplot(aes(x = tt, y = id2, colour = p)) + 
   geom_vline(xintercept = 0, alpha = .25) +
   geom_point(size=.5) + 
-  scale_y_continuous(breaks = scales::pretty_breaks(18)) +
+  scale_y_continuous(breaks = scales::pretty_breaks(length(change20s))) +
   scale_x_continuous(breaks = scales::pretty_breaks(10)) +
   scale_color_manual(values = c('black', 'red')) + 
   theme_csda() + 
   labs(x = 'Time (years) from kidney graft failure (0: failure time)', 
-       y = 'ID', colour = 'Proterinuria')
+       y = 'ID', colour = 'Proterinuria') + 
+  guides(colour = guide_legend(override.aes = list(size=2)))
 
-prot <- last_plot()
+ggsave('./KidneyBinaryTrajectories.png', width = 140, height = 60, units = 'mm', dpi = 1e3)
 
 
-ggpubr::ggarrange(gfrplot, haeplot, prot)
