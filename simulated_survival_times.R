118e076a388a4c40e85c37426195919adf43cac3
jamesmurray7 <j.murray7@ncl.ac.uk>
2022-02-11 14:20:56 +0000

    seeing if i can get gamma,eta a bit more compelling-looking

--- simulated_survival_times.R
+++ simulated_survival_times.R
@@ -26,7 +26,7 @@ for(i in 1:nrow(thetas)){
 }
 
 survtimes <- lapply(sets, function(x){
-  do.call(c, lapply(x, function(y) sort(unique(y[y$cens==1, 'survtime']))))
+  do.call(c, lapply(x, function(y) sort(y[y$cens==1, 'survtime'])))
 })
 
 step1 <- do.call(c, survtimes) %>% as.data.frame() %>% 
@@ -70,27 +70,28 @@ ggsave('alpha_nu_failure.png', width = 140, height = 90, units = 'mm')
 rm(list=ls())
 source('theme_csda.R')
 # Hold baseline hazard constant at nu = -4
-theta0 <- -4; theta1 <- 0.25;
+theta0 <- -6; theta1 <- 0.001;
 gamma.eta <- expand.grid(
   gamma = c(-1.0, -0.5, 0.0, 0.5, 1.0),
   eta1 = c(0.05),
   eta2 = c(-1.0, -0.3, 0.3, 1.0)
 )
+# D = diag(c(0.5^2, 0.25^2, 0.5^2, 0.25^2))
 
 sets <- list()
 for(i in 1:nrow(gamma.eta)){
   gamma <- c(gamma.eta[i, 1], 0)
-  eta <- c(0.05, gamma.eta[i, 3])
+  eta <- c(0.0, gamma.eta[i, 3])
   sets[[i]] <- replicate(100,
-                         simData(ntms = 10, gamma.x = eta, gamma.y = gamma, theta0 = theta0, theta1 = theta1,
-                                 D = diag(c(0.5^2, 0.25^2, 0.5^2, 0.25^2)))$survdat,
+                         joineR::simjoint(n = 100, ntms = 10, b2 = eta, gamma = gamma.eta[i,1], theta0=-6,theta1=1e-2,truncation = T,trunctime=9.1)$survi,
+                       #  simData(ntms = 10, gamma.x = eta, gamma.y = gamma, theta0 = theta0, theta1 = theta1)$survdat,
                          simplify = F
   )
   names(sets)[i] <- paste0('gamma == ', gamma.eta[i, 1], ', eta[2] == ', eta[2])
 }
 
 survtimes <- lapply(sets, function(x){
-  do.call(c, lapply(x, function(y) sort(unique(y[y$cens==1, 'survtime']))))
+  do.call(c, lapply(x, function(y) sort(y[y$cens==1, 'survtime'])))
 })
 
 step1 <- do.call(c, survtimes) %>% as.data.frame() 
@@ -134,7 +135,7 @@ step2 %>%
   scale_x_continuous(breaks = scales::pretty_breaks(10)) + 
   labs(x = 'Simulated failure time', y = '')
 
-ggsave('gamma_eta_failure.png', width = 140, height = 90, units = 'mm')
+ggsave('gamma_eta_failure2.png', width = 140, height = 90, units = 'mm')
 
 
 # Altering magnitude of vech(D) -------------------------------------------
@@ -150,23 +151,25 @@ int.slope <- expand.grid(
 )
 
 sets <- list()
-for(i in 1:nrow(gamma.eta)){
+for(i in 1:nrow(int.slope)){
   D <- diag(c(int.slope[i, 1], int.slope[i, 2], .01, .01))
   sets[[i]] <- replicate(100,
                          simData(ntms = 10, gamma.x = eta, gamma.y = gamma, theta0 = theta0, theta1 = theta1,
                                  D = D)$survdat,
                          simplify = F
   )
-  names(sets)[i] <- paste0('gamma == ', gamma.eta[i, 1], ', eta[2] == ', eta[2])
+  names(sets)[i] <- paste0('D11 == ', int.slope[i, 1], ', D22 == ', int.slope[i, 2])
 }
 
 survtimes <- lapply(sets, function(x){
-  do.call(c, lapply(x, function(y) sort(unique(y[y$cens==1, 'survtime']))))
+  do.call(c, lapply(x, function(y) sort(y[y$cens==1, 'survtime'])))
 })
 
 step1 <- do.call(c, survtimes) %>% as.data.frame() 
 
 step1$id <- do.call(c, lapply(names(survtimes), function(x) rep(x, each = length(survtimes[[x]]))))
+
+names(step1) <- c('survtime','id')
 
 step2 <- step1 %>% 
   as_tibble %>% 
@@ -195,10 +198,12 @@ step2 %>%
   theme_csda() + 
   theme(
     strip.placement = 'outside',
-    strip.text = element_text(size = 9),
+    strip.text = element_text(size = 8),
     axis.text.x = element_text(size = 8),
     axis.ticks.y = element_blank(),
     axis.text.y = element_blank()
   ) + 
   scale_x_continuous(breaks = scales::pretty_breaks(10)) + 
   labs(x = 'Simulated failure time', y = '')
+
+ggsave('int_slope_failure.png', width = 140, height = 90, units = 'mm')
