80da8a130aa57bf18d78cc592212b84fbce03671
jamesmurray7 <j.murray7@ncl.ac.uk>
2023-09-12 09:53:31 +0100

    finishing up

--- thesis-app/PBC/multiJMs.R
+++ thesis-app/PBC/multiJMs.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 source(".Rprofile")
 source("helpers.R")
-save.dir <- save.dir.file.path("Joint")
+out.dir <- save.dir.file.path("Joint")
 library(splines)
 
 # Gaussians ---------------------------------------------------------------
@@ -25,11 +25,11 @@ Ps <- plotMultiJM(Gs)
 
 # I think put the survival ones in main-text
 Ps$Surv
-ggsave(save.dir.file.path("GaussiansSurv.png", save.dir),
+ggsave(save.dir.file.path("GaussiansSurv.png", out.dir),
        width = 140, height = 90, units = "mm")
 # and the longitudinals in appendix.
 Ps$Long
-ggsave(save.dir.file.path("GaussiansLong.png", save.dir),
+ggsave(save.dir.file.path("GaussiansLong.png", out.dir),
        width = 140, height = 120, units = "mm")
 
 # Counts ------------------------------------------------------------------
@@ -54,10 +54,10 @@ summary(Cs)
 Pc <- plotMultiJM(Cs)
 
 Pc$Surv
-ggsave(save.dir.file.path("CountsSurv.png", save.dir),
+ggsave(save.dir.file.path("CountsSurv.png", out.dir),
        width = 140, height = 90, units = "mm")
 Pc$Long
-ggsave(save.dir.file.path("CountsLong.png", save.dir),
+ggsave(save.dir.file.path("CountsLong.png", out.dir),
        width = 140, height = 90, units = "mm")
 
 # + DispModels ------------------------------------------------------------
@@ -84,10 +84,10 @@ summary(C.Disps)
 PC.Disps <- plotMultiJM(C.Disps, 6)
 
 PC.Disps$Surv
-ggsave(save.dir.file.path("DispCountsSurv.png", save.dir),
+ggsave(save.dir.file.path("DispCountsSurv.png", out.dir),
        width = 140, height = 90, units = "mm")
 PC.Disps$Long
-ggsave(save.dir.file.path("DispCountsLong.png", save.dir),
+ggsave(save.dir.file.path("DispCountsLong.png", out.dir),
        width = 140, height = 180, units = "mm")
 
 
@@ -111,10 +111,10 @@ plot(Conts.cc)
 
 Pconts <- plotMultiJM(Conts) # -> And then we drop {SGOT, prothrombin}
 Pconts$Surv
-ggsave(save.dir.file.path("ContsSurv.png", save.dir),
+ggsave(save.dir.file.path("ContsSurv.png", out.dir),
        width = 140, height = 90, units = "mm")
 Pconts$Long
-ggsave(save.dir.file.path("ContsLong.png", save.dir),
+ggsave(save.dir.file.path("ContsLong.png", out.dir),
        width = 140, height = 180, units = "mm")
 
 
@@ -136,10 +136,10 @@ plot(Conts.reduced.cc)
 
 Pconts.reduced <- plotMultiJM(Conts.reduced)
 Pconts.reduced$Surv
-ggsave(save.dir.file.path("ContsReducedSurv.png", save.dir),
+ggsave(save.dir.file.path("ContsReducedSurv.png", out.dir),
        width = 140, height = 90, units = "mm")
 Pconts.reduced$Long
-ggsave(save.dir.file.path("ContsReducedLong.png", save.dir),
+ggsave(save.dir.file.path("ContsReducedLong.png", out.dir),
        width = 140, height = 90, units = "mm")
 
 
@@ -160,7 +160,7 @@ fin <- joint(
   family = list("gaussian", "gaussian", "negbin", "genpois", "binomial"),
   disp.formulas = list(~1, ~1, ~time+age+drug+histologic, ~time+age+histologic+sex, ~1)
 )
-save(fin, file = save.dir.file.path("fin.RData", save.dir))
+save(fin, file = save.dir.file.path("fin.RData", out.dir))
 
 # Without hepa?
 fin2 <- joint(
@@ -177,19 +177,46 @@ fin2 <- joint(
   family = list("gaussian", "gaussian", "negbin", "genpois"),
   disp.formulas = list(~1, ~1, ~time+age+drug+histologic, ~time+age+histologic+sex)
 ) 
-save(fin2, file = save.dir.file.path("fin2.RData", save.dir))
+save(fin2, file = save.dir.file.path("fin2.RData", out.dir))
 # Looks better!!!
 
 fin2.cc <- cond.ranefs(fin2, tune = .425)
-png(filename = save.dir.file.path("test.png", save.dir), # same as earlier plots really...
+png(filename = save.dir.file.path("test.png", out.dir),  # same as earlier plots really...
     units = "mm", width = 400, height = 400, res = 500)  # which is good as too big!
 plot(fin2.cc)
 dev.off()
 
 Pfin2 <- plotMultiJM(fin2, 10)
 Pfin2$Surv
-ggsave(save.dir.file.path("fin2Surv.png", save.dir),
+ggsave(save.dir.file.path("fin2Surv.png", out.dir),
        width = 140, height = 90, units = "mm")
 Pfin2$Long
-ggsave(save.dir.file.path("fin2Long.png", save.dir),
+ggsave(save.dir.file.path("fin2Long.png", out.dir),
        width = 140, height = 185, units = "mm")
+
+
+# Final 3 -----------------------------------------------------------------
+fin.fin <- joint(
+  long.formulas = list(
+    # Continuous k=1,2
+    albumin ~ age + histologic + time + I(time^2) + (1 + time + I(time^2)|id),
+    serBilir ~ sex + histologic + ns(time, knots = c(1, 4)) + (1 + ns(time, knots = c(1, 4))|id)
+  ),
+  surv.formula = Surv(survtime, status) ~ age + histologic,
+  data = PBCredx,
+  family = list("gaussian", "gaussian")
+)
+
+fin.fin2 <- joint( # strangely(?) this is half as fast!?
+  long.formulas = list(
+    # Continuous k=1,2
+    albumin ~ age + histologic + time + I(time^2) + (1 + time + I(time^2)|id),
+    serBilir ~ sex + histologic + ns(time, knots = c(1, 4)) + (1 + ns(time, knots = c(1, 4))|id)
+  ),
+  surv.formula = Surv(survtime, status) ~ age,
+  data = PBCredx,
+  family = list("gaussian", "gaussian")
+)
+
+
+
