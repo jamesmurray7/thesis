a11a01596ff70a605649d626b3e8b4ae07485b32
jamesmurray7 <j.murray7@ncl.ac.uk>
2023-10-19 15:07:00 +0100

    Nice plot

--- thesis-app/PBC/finalmodels.R
+++ thesis-app/PBC/finalmodels.R
@@ -12,13 +12,18 @@ gmv.dir <- save.dir.file.path("Joint/Multivs")
 vech <- gmvjoint:::vech
 qz <- qnorm(.975)
 
+.xx <- function(x){
+  substr(x,1,1)<- toupper(substr(x,1,1))
+  x
+}
+
 
 tab.from.joineRML <- function(G, M){
   # Check
   stopifnot(inherits(G, "joint") && inherits(M, "mjoint"))
   # summary.mjoint returns Estimate (SE) [95% CI] for {beta, sigma^2, gamma, zeta}
   SM <- summary(M)
-  VarM <- vcov(M)
+  VarM <- joineRML:::vcov.mjoint(M)
   # Work out vech(D)
   jML.D <- SM$D
   jML.vD <- vech(SM$D)
@@ -131,20 +136,23 @@ tab.from.JMbayes2 <- function(G, J){
 # TABULATIONS -------------------------------------------------------------
 # We want to create the following table for trivariate and subseq. bivariate 
 # joint models fit to PBC.
-create.table <- function(ww = c("triv", "biv")){
+create.table <- function(ww = c("triv", "biv"), reduced = FALSE){
   # Loading (local assignment)
+  suff <- if(reduced) 2 else NULL
   if(ww == "triv"){
     # `Jmbayes2`
     assign("J", get(load(save.dir.file.path("Triv.RData", jmb.dir))))
     # `Gmvjoint`
     assign("G", get(load(save.dir.file.path("triv.RData", gmv.dir))))
   }else{
+    file.name <- paste0("biv", suff, ".RData")
     # `Jmbayes2`
-    assign("J", get(load(save.dir.file.path("Biv.RData", jmb.dir))))
+    assign("J", get(load(save.dir.file.path(.xx(file.name), jmb.dir))))
     # `Gmvjoint`
-    assign("G", get(load(save.dir.file.path("biv.RData", gmv.dir))))
+    assign("G", get(load(save.dir.file.path(file.name, gmv.dir))))
     # `joinerMl`
-    assign("M", get(load(save.dir.file.path("bivjML.RData", gmv.dir))))
+    file.name <- gsub("\\.RData", "jML.RData", file.name)
+    assign("M", get(load(save.dir.file.path(file.name, gmv.dir))))
   }
 
   to.xt2 <- do.call(rbind, tab.from.JMbayes2(G, J))
@@ -161,16 +169,113 @@ create.table <- function(ww = c("triv", "biv")){
         sanitize.text.function = identity)
 }
 
-create.table("triv")
-create.table("biv")
+create.table("triv", FALSE)
+# Before removing sex from surv. model
+create.table("biv", FALSE)
+# After removing sex from surv. model
+create.table("biv", TRUE)
 
 
 # FIGURE(S) ---------------------------------------------------------------
-# Want to make a plot for survival (?) part only (?: maybe do everything)
+# Create _one_ for the final bivariate joint model...
+load(save.dir.file.path("Biv2.RData", jmb.dir)) # `J3`
+load(save.dir.file.path("biv2.RData", gmv.dir)) # `joint.biv2`
+load(save.dir.file.path("biv2jML.RData", gmv.dir)) # `joint.biv2.joineRML`
+
+# This gets everything needed, just need to extract relevant stuff!
+one <- do.call(rbind, tab.from.JMbayes2(joint.biv2, J3))
+two <- do.call(rbind, tab.from.joineRML(joint.biv2, joint.biv2.joineRML))
+
+all3 <- as.data.frame(rbind(one[,1:3],                              # Approx. EM
+      cbind(one[1:nrow(one), 1], one[,5:6]),  # JMbayes2
+      cbind(two[1:nrow(two), 1], two[,5:6])   # joineRML
+))
+
+all3$Method <- c(rep("Approximate EM", nrow(one)), 
+                 rep("JMbayes2", nrow(one)),
+                 rep("joineRML", nrow(two)))
+
+all3$Method <- factor(all3$Method, unique(all3$Method))
+
+# Let's get wrangling and plotting...
+library(tidyverse)
+
+# Fixing parameter names
+fix.names <- function(name.vec){
+  # D // D_{k,ef} -> D[k,ef]
+  # beta // beta_{kp} -> beta[kp]
+  # sigma // sigma^2_k -> sigma[2]^2
+  # gamma // gamma_k -> gamma[k]
+  # zeta // zeta_p -> zeta[p]
+  sapply(name.vec, function(x){  
+    if(grepl("^D\\_\\{", x)){
+      bit.in.braces <- str_extract(x, "\\{(.*?)\\}") %>% gsub("\\{|\\}", "", .)
+      before.com <- str_extract(bit.in.braces, "^.*?\\,") %>% gsub("\\,", "", .)
+      after.com <- str_extract(bit.in.braces, "\\,.*?$") %>% gsub("\\,", "", .)
+      param <- str_extract(x, "^.*?\\_") %>% gsub("\\_", "", .)
+      return(paste0(param, "[", before.com, '*","*', after.com, ']'))
+    }else if(grepl("^beta\\_", x)){
+      bit.in.braces <- str_extract(x, "\\{(.*?)\\}") %>% gsub("\\{|\\}", "", .)
+      param <- str_extract(x, "^.*?\\_") %>% gsub("\\_", "", .)
+      return(paste0(param, "[~~", bit.in.braces, ']'))
+    }else if(grepl("^sigma", x)){
+      return(paste0("sigma[~~", str_extract(x, "\\d$"), ']^{~~2}'))
+    }else{
+      return(paste0(str_extract(x, "^.*?\\_") %>% gsub("\\_", "", .),
+                    "[~~", str_extract(x, "\\d$"), "]"))
+    }
+  })
+}
+all3$Parameter2 <- fix.names(all3$Parameter)
+all3$Parameter2 <- gsub('00\\]', '"00"]',all3$Parameter2) # Weird behaviour of expression, turn to string...
+
+# Extracting point estimate
+all3$Estimate <- str_extract(all3$MSE %>% trimws, "^.*?\\s") %>% as.numeric
+
+# Separating lower and upper bands
+all3b <- separate(all3, CI, c("lb", "ub"), "\\,+", remove = F) %>% 
+  mutate_at("lb", ~ gsub("^\\[", "", .x) %>% as.numeric) %>% 
+  mutate_at("ub", ~ gsub("\\]$", "", .x) %>% as.numeric) %>% 
+  mutate_at("Parameter2", fct_inorder)
+
+# Groupings?
+all3b <- all3b %>% 
+  mutate(
+    grouping = case_when(
+      grepl("^D\\[", Parameter2) ~ "Covariance matrix",
+      grepl("^beta", Parameter2) ~ "Fixed effects",
+      grepl("^sigma", Parameter2) ~ "Residual variance",
+      T ~ "Survival parameters"
+    )
+  )
+
+all3b %>% 
+  ggplot(aes(x = fct_rev(Parameter2), y =  Estimate, colour = Method)) + 
+  geom_pointrange(aes(ymin = lb, ymax = ub),
+                  position = position_dodge(width = 0.33),
+                  size = .25, lwd = .25) +
+  scale_x_discrete(labels = function(l) parse(text=l)) + 
+  theme_csda() +
+  coord_flip()
 
 
+all3b %>% 
+  ggplot(aes(x = fct_rev(Parameter2), y =  Estimate, colour = Method)) + 
+  geom_pointrange(aes(ymin = lb, ymax = ub),
+                  position = position_dodge(width = 0.33),
+                  size = .1, lwd = .25) +
+  scale_x_discrete("", labels = function(l) parse(text = l)) + 
+  facet_wrap(~grouping, scales = 'free') + 
+  # scale_colour_brewer(palette = "YlOrRd") + 
+  # Shift YlOrRd because hard to see!
+  scale_colour_manual(values = c("#FECC5C", "#FD8D3C", "#F03B20", "#BD0026")) +
+  theme_csda() +
+  theme(
+    strip.text = element_text(vjust = 1, size = 7),
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    legend.text = element_text(size=6)
+  ) + 
+  coord_flip()
 
-
-
-
-
+ggsave("FinalModelAllMethods.png", width = 140, height = 110, units = "mm")  
